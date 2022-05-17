f3abc9576d3632bef5e0e947719143020c1086ec
sport1028 <sport1028kr@naver.com>
2022-05-17 22:48:42 +0900

    init commit

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# mobile-invitation from 2022-05-17
+mobile-invitation for my wedding
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './img/logo-removebg.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="loading-screen">
+      <header className="loading-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
